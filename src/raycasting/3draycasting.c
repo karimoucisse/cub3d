b59f7f099c68c570059875d5345e969a4602b4d3
karimou cisse <karimou.cisse1@gmail.com>
2025-04-10 12:28:43 +0200

    add change in code

--- src/raycasting/3draycasting.c
+++ src/raycasting/3draycasting.c
@@ -16,58 +16,55 @@ void	draw_vertical_line(t_game *game, int x, double angle)
 	}
 }
 
-// void	render_3d_map(t_game *game, int i, double angle)
-// {
-// 	double	project_plane;
-
-// 	project_plane = (WIDTH / 2) / tan(FOV / 2);
-// 	game->raycast_info.wall_height = (TILE / game->raycast_info.wall_hit_dist)
-// 		* project_plane;
-// 	draw_vertical_line(game, i, angle);
-// }
-
 void	render_3d_map(t_game *game, t_texture texture, int x)
 {
+	double	wall_top_pixel;
+	double	wall_bottom_pixel;
 	int		y;
-	int		texture_pos_initial;
-	int		texture_y;
+	int		texture_offset_x;
+	int		distanceFromTop;
+	int		texture_offset_y;
 	int		color;
-	double	step;
-	double	compteur;
 	double	project_plane;
-	double	wall_start;
 
 	project_plane = (WIDTH / 2) / tan(FOV / 2);
 	game->raycast_info.wall_height = (TILE / game->raycast_info.wall_hit_dist)
 		* project_plane;
-	if (!game->raycast_info.was_hit_vertical)
-		texture_pos_initial = (int)((int)game->raycast_info.wall_hitx % TILE)
-			* texture.width / TILE;
-	else
-		texture_pos_initial = (int)((int)game->raycast_info.wall_hity % TILE)
-			* texture.width / TILE;
-	step = (double)texture.height / game->raycast_info.wall_height;
-	wall_start = (HEIGHT / 2) - (game->raycast_info.wall_height / 2);
-	compteur = (wall_start < 0) ? (-wall_start * step) : 0.0;
+	wall_top_pixel = (HEIGHT / 2) - (game->raycast_info.wall_height / 2);
+	if (wall_top_pixel < 0)
+		wall_top_pixel = 0;
+	wall_bottom_pixel = (HEIGHT / 2) + (game->raycast_info.wall_height / 2);
+	if (wall_bottom_pixel > HEIGHT)
+		wall_bottom_pixel = HEIGHT;
 	y = 0;
+	// ceilling color
+	while (y < wall_top_pixel)
+	{
+		ft_put_pixel(x, y, 0xFFF000, game);
+		y++;
+	}
+	if (game->raycast_info.was_hit_vertical)
+		texture_offset_x = (int)game->raycast_info.wall_hity % TILE;
+	else
+		texture_offset_x = (int)game->raycast_info.wall_hitx % TILE;
+	y = wall_top_pixel;
+	// render texture wall
+	while (y < wall_bottom_pixel)
+	{
+		distanceFromTop = y + (game->raycast_info.wall_height / 2) - (HEIGHT
+				/ 2);
+		texture_offset_y = distanceFromTop * ((double)texture.height
+				/ game->raycast_info.wall_height);
+		color = texture.pixels[(texture.width * texture_offset_y)
+			+ texture_offset_x];
+		ft_put_pixel(x, y, color, game);
+		y++;
+	}
+	y = wall_bottom_pixel;
+	// floor color
 	while (y < HEIGHT)
 	{
-		if (y > (HEIGHT / 2) - (game->raycast_info.wall_height / 2)
-			&& y < (HEIGHT / 2) + (game->raycast_info.wall_height / 2))
-		{
-			texture_y = (int)compteur % texture.height;
-			compteur += step;
-			color = texture.pixels[texture_y * texture.width
-				+ texture_pos_initial];
-		}
-		else
-		{
-			if (y < (HEIGHT / 2))
-				color = 0x00FFF0;
-			else
-				color = 0xFFF000;
-		}
-		ft_put_pixel(x, y, color, game);
+		ft_put_pixel(x, y, 0x00FFF0, game);
 		y++;
 	}
 }
